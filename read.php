04100aebe1cde1bac7d2a95e5f3dab917780b56a
Thijmen Stegenga <t.j.stegenga@gmail.com>
2021-09-08 11:53:23 +0200

    new document
    Added a read.hph

---
+++ read.php
@@ -0,0 +1,12 @@
+<?php
+   function getList(){
+            $conn = databaseConnection();
+            $query = $conn->prepare("SELECT * FROM `to-do`");
+            $query->execute();
+            $list = $query->fetchall();
+            return $list;
+        }
+
+
+
+?>
