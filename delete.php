95253b8c4cf78c863bdd4523c5db174b30c7d7aa
Thijmen Stegenga <t.j.stegenga@gmail.com>
2021-09-10 11:53:13 +0200

    added font awesome icons and buttons

--- delete.php
+++ delete.php
@@ -4,10 +4,12 @@
     function deleteNames(){
         $conn = databaseConnection();
         $query = $conn->prepare("DELETE FROM `to-do` (name) VALUES (:name) WHERE id = :id ");
-        $query->bindParam(":id" , $_POST['id'])
+        $query->bindParam(":id" , $_POST['id']);
         $query->bindParam(":name", $_POST['name']);
         $query->execute();
 
         deleteNames();
+
+        //header( "Location: index.php" );
     }
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 	require("db-connect.php"); 
-	require("read.php");
+	include("read.php");
 	databaseConnection();
 ?> 
 
@@ -11,6 +11,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="STYLESHEET" href="style.css" type="text/css">
+	<script src="https://kit.fontawesome.com/df923f75a1.js" crossorigin="anonymous"></script>
 	<title>Document</title>
 </head>
 <body>
@@ -25,6 +26,9 @@
 	  foreach ($list as $listitem){
             //var_dump($listitem); ?>
             <h2><?php echo $listitem['name'] ?></h2>
+            <a href="delete.php?id=<?php echo $listitem['id'] ?> "class="btn btn-dark"><i class="far fa-edit"></i></i></a>
+            <a href="update.php?id=<?php echo $listitem['id'] ?> "class="btn btn-dark"><i class="fas fa-trash-alt"></i></a>
+
             <?php
         }
         ?>
