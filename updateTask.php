426cbeb2556bcd4d4c36a1a46e562ac8d5f5ba41
Thijmen Stegenga <t.j.stegenga@gmail.com>
2021-09-17 12:11:20 +0200

    added form for the update page

---
+++ updateTask.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Update List</title>
+</head>
+<body>
+		<form name="update" method="post" action="update.php">
+			<div class="form-group d-flex justify-content-center">
+				<input type="hidden" name="id" value="<?=$id[id] ?>"/>
+				<label for="">Edit list name</label>
+				<input class="form-control w-50 " type="text" name="name" placeholder="Vul naam in" required><br>
+			</div>
+			<input type="submit" class="btn btn-info">
+		</form>
+
+</body>
+</html>
+
